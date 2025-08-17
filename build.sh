2598cc7e0c156ed32c8c0f8bd3cfa67c374de5e5
Nandan <nandu.k.0312@gmail.com>
2025-08-17 15:05:40 +0530

    Updating build.sh file

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 
 echo "building the images..."
 
-if (env.BRANCH_NAME == 'dev') {
-    sh "docker build -t nandoo03/dev:latest ."
-} 
-else if (env.BRANCH_NAME == 'master') {
-    sh "docker build -t nandoo03/prod:latest ."
-}
+if [ "$BRANCH_NAME" = "dev" ]; then
+    docker build -t nandoo03/dev:latest .
+elif [ "$BRANCH_NAME" = "master" ]; then
+    docker build -t nandoo03/prod:latest .
+fi
